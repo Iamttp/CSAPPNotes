81c006342319f5812214fb100fd06b7b58303ae2
ttp <1877784332@qq.com>
2019-11-13 10:39:09 +0800

    u

--- Cprogram/test1.c
+++ Cprogram/test1.c
@@ -1,9 +1,5 @@
 #include<stdio.h>
 
-int sum_test(int a,int b){
-    return a+b;
-}
-
 long absdiff(long x,long y){
     long result;
     if(x > y)
@@ -11,6 +7,24 @@ long absdiff(long x,long y){
     else
         return y - x;
     return result;
+}
+
+// absdiff 的汇编写法
+long absdiff_j(long x,long y){
+    long result;
+    int ntest = x <= y;
+    if(ntest)
+        goto Else;
+    result = x - y;
+    goto Done;
+Else:
+    result = y - x;
+Done:
+    return result;
+}
+
+int sum_test(int a,int b){
+    return a+b;
 }
 
 int gt(long x,long y){
--- Cprogram/test1.s
+++ Cprogram/test1.s
@@ -1,48 +1,67 @@
 	.file	"test1.c"
 	.text
-	.globl	sum_test
-	.type	sum_test, @function
-sum_test:
-.LFB23:
-	.cfi_startproc
-	leal	(%rdi,%rsi), %eax
-	ret
-	.cfi_endproc
-.LFE23:
-	.size	sum_test, .-sum_test
 	.globl	absdiff
 	.type	absdiff, @function
 absdiff:
-.LFB24:
+.LFB23:
 	.cfi_startproc
 	cmpq	%rsi, %rdi
-	jg	.L5
+	jg	.L4
 	movq	%rsi, %rax
 	subq	%rdi, %rax
 	ret
-.L5:
+.L4:
+	movq	%rdi, %rax
+	subq	%rsi, %rax
+	ret
+	.cfi_endproc
+.LFE23:
+	.size	absdiff, .-absdiff
+	.globl	absdiff_j
+	.type	absdiff_j, @function
+absdiff_j:
+.LFB24:
+	.cfi_startproc
+	cmpq	%rsi, %rdi
+	jg	.L8
+.L6:
+	movq	%rsi, %rax
+	subq	%rdi, %rax
+.L7:
+	ret
+.L8:
 	movq	%rdi, %rax
 	subq	%rsi, %rax
 	ret
 	.cfi_endproc
 .LFE24:
-	.size	absdiff, .-absdiff
+	.size	absdiff_j, .-absdiff_j
+	.globl	sum_test
+	.type	sum_test, @function
+sum_test:
+.LFB25:
+	.cfi_startproc
+	leal	(%rdi,%rsi), %eax
+	ret
+	.cfi_endproc
+.LFE25:
+	.size	sum_test, .-sum_test
 	.globl	gt
 	.type	gt, @function
 gt:
-.LFB25:
+.LFB26:
 	.cfi_startproc
 	cmpq	%rsi, %rdi
 	setg	%al
 	movzbl	%al, %eax
 	ret
 	.cfi_endproc
-.LFE25:
+.LFE26:
 	.size	gt, .-gt
 	.globl	arith
 	.type	arith, @function
 arith:
-.LFB26:
+.LFB27:
 	.cfi_startproc
 	leaq	(%rdi,%rsi), %rax
 	addq	%rdx, %rax
@@ -52,7 +71,7 @@ arith:
 	imulq	%rdx, %rax
 	ret
 	.cfi_endproc
-.LFE26:
+.LFE27:
 	.size	arith, .-arith
 	.section	.rodata.str1.1,"aMS",@progbits,1
 .LC0:
@@ -61,7 +80,7 @@ arith:
 	.globl	main
 	.type	main, @function
 main:
-.LFB27:
+.LFB28:
 	.cfi_startproc
 	subq	$8, %rsp
 	.cfi_def_cfa_offset 16
@@ -78,7 +97,7 @@ main:
 	.cfi_def_cfa_offset 8
 	ret
 	.cfi_endproc
-.LFE27:
+.LFE28:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 7.4.0-1ubuntu1~18.04.1) 7.4.0"
 	.section	.note.GNU-stack,"",@progbits
